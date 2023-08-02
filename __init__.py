5b46169dd305fbab8beb8dedea7ffb5d67a98577
Luc Mcgrady <lucmcgrady@gmail.com>
2023-08-02 20:17:06 +0100

    Added: Html replacement

--- __init__.py
+++ __init__.py
@@ -1,16 +1,33 @@
 from anki.hooks import wrap
+
 from aqt.stats import NewDeckStats
+from aqt.qt import QUrl
+from aqt import mw
 
-with open("injector.js") as f:
-    injectorJs = f.read()
-
+with open("replacement.html") as f:
+    html = f.read()
 with open("graphs_append.js") as f:
     innerJs = f.read()
 
-js = injectorJs % innerJs.replace("\n", ";").replace("\"", "\\\"")
+html = html.format(
+    js=innerJs.replace("\n", ";").replace("\"", "\\\""),
+    media=mw.serverURL()
+)
 
-def newRefresh(self: NewDeckStats):
-    print(js)
-    self.form.web.eval(js)
+def new_refresh(self: NewDeckStats):
+    
+    old_load_url = self.form.web.load_url
 
-NewDeckStats.refresh = wrap(NewDeckStats.refresh, newRefresh, "before")
+    def fake_load(url: QUrl):
+        print(url.url())
+
+        if "graphs" in url.url(): # Without this it overflows as setHtml calls load_url
+            self.form.web.setHtml(html)
+            print(html)
+        else:
+            print(url.url())
+            old_load_url(url)
+
+    self.form.web.load_url = fake_load
+
+NewDeckStats.refresh = wrap(NewDeckStats.refresh, new_refresh, "before")
--- injector.js
+++
@@ -1,5 +0,0 @@
-const script = document.createElement("script")
-script.innerHTML="%s"
-script.defer = true
-
-document.head.appendChild(script)
