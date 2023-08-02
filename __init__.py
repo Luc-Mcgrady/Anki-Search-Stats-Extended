37141e3194f5a79565ff08f14f99ec7045c896b3
Luc Mcgrady <lucmcgrady@gmail.com>
2023-08-02 14:00:06 +0100

    Added: Anki output

--- __init__.py
+++ __init__.py
@@ -1,7 +1,10 @@
 from anki.hooks import wrap
 from aqt.stats import NewDeckStats
 
+with open("graphs_append.js") as f:
+    js = f.read()
+
 def newRefresh(self: NewDeckStats):
-    self.form.web.setHtml("Hello World")
+    self.form.web.eval(js)
 
 NewDeckStats.refresh = wrap(NewDeckStats.refresh, newRefresh)
---
+++ graphs_append.js
@@ -0,0 +1,1 @@
+document.body.innerHTML = JSON.stringify(anki)
