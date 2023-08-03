2ee65329008e0f555a926f82410a108354ed9c8e
Luc Mcgrady <lucmcgrady@gmail.com>
2023-08-03 21:28:59 +0100

    Fix: Error when addon isn't cwd

--- __init__.py
+++ __init__.py
@@ -1,9 +1,12 @@
 from anki.hooks import wrap
 
 from aqt.stats import NewDeckStats
+import os.path
+
+addon_dir = os.path.dirname(__file__)
 
 def new_refresh(self: NewDeckStats):
-    with open("graphs.min.js") as f: # Putting this inside the function allows you to rebuild the page with anki still open
+    with open(f"{addon_dir}/graphs.min.js") as f: # Putting this inside the function allows you to rebuild the page with anki still open
         innerJs = f.read()
 
     self.form.web.eval(innerJs)
