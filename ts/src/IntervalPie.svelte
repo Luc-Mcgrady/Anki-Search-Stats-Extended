4abac94e2a5c67fec1dbe02de7b3c7e1d66f09b5
Luc Mcgrady <lucmcgrady@gmail.com>
2023-08-07 12:04:51 +0100

    Added: Interval pie

--- ts/src/IntervalPie.svelte
+++ ts/src/IntervalPie.svelte
@@ -3,28 +3,57 @@
     import Graph from "./Graph.svelte"
     import Pie from "./Pie.svelte"
     import type {PieDatum} from "./pie"
-    import { defaultGraphBounds } from "./graph";
-    
-    export let data: GraphsResponse
+    import _ from "lodash"
+    import Rainbow from "rainbowvis.js"
 
-    const bounds = defaultGraphBounds()
-    
-    const intervals = data.intervals
+    export let data: GraphsResponse 
 
-    const pie_data : PieDatum[] = [{
-        label: "test1",
-        value: 10,
-        colour: "red"
-    },
-    {
-        label: "test2",
-        value: 40,
-        colour: "green"
-    }] 
+    const gradient = new Rainbow()
+    
+    let step = 4;
+    const steps = 6;
+    
+    const intervals = data.intervals!.intervals
+    $: last = step*steps
+
+    gradient.setNumberRange(0,steps + 1)
+    gradient.setSpectrum("#74C476", "#31a354")
+
+    let pie_data: PieDatum[]
+    $: pie_data = 
+        [..._.range(0,step*steps,step)
+        .map((start,i)=>
+            {
+                const end = start+step
+
+                const count=
+                    _.range(start, end+1)
+                    .reduce((n,j)=>n+(intervals[j] || 0))
+
+                return {
+                    label: `${start}-${end}`,
+                    value: count,
+                    colour: `#${gradient.colourAt(i)}`
+                }
+            }
+        ), 
+        { // The to infinity one
+            label: `${last}-Infinity`,
+            value: 
+                Object.entries(intervals)
+                .filter(([i, _])=>parseInt(i)>last)
+                .reduce((n,[_, v])=>n+v, 0)
+            ,
+            colour: `#${gradient.colourAt(steps+1)}`
+        }
+    ]
+    
 </script>
 
 <Graph>
-    <h1>Interval pie</h1>
+    <h1>Interval Distribution</h1>
     <hr/>
     <Pie data={pie_data}></Pie>
+    <label for="interval_pie_step"></label>
+    <input type="number" id="interval_pie_step" bind:value={step}>
 </Graph>
