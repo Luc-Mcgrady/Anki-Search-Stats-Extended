6f23c431cdc6cfa444884874e146089b4b963710
Luc Mcgrady <lucmcgrady@gmail.com>
2023-08-07 12:35:46 +0100

    Added: Last day counter

--- ts/src/IntervalPie.svelte
+++ ts/src/IntervalPie.svelte
@@ -9,17 +9,19 @@
     export let data: GraphsResponse 
 
     
-    let step = 6;
     let steps = 4;
+    let last = 21;
+    
+    $: step = Math.ceil(last / steps);
+    $: realLast = steps * step
     
     const intervals = data.intervals!.intervals
-    $: last = step*steps
 
     const gradient = new Rainbow()
 
     $ : {
         gradient.setNumberRange(0,steps + 1)
-        gradient.setSpectrum("#74C476", "#31a354")
+        gradient.setSpectrum("#74C476", "#014720")
     }
 
     let pie_data: PieDatum[]
@@ -41,13 +43,13 @@
             }
         ), 
         { // The to infinity one
-            label: `${last}-Infinity`,
+            label: `${realLast}-Infinity`,
             value: 
                 Object.entries(intervals)
-                .filter(([i, _])=>parseInt(i)>last)
+                .filter(([i, _])=>parseInt(i)>realLast)
                 .reduce((n,[_, v])=>n+v, 0)
             ,
-            colour: `#${gradient.colourAt(steps+1)}`
+            colour: "grey"
         }
     ]
     
@@ -58,8 +60,8 @@
     <hr/>
     <Pie data={pie_data}></Pie>
     <div>
-        <label for="interval_pie_step">Days per step</label>
-        <input type="number" id="interval_pie_step" bind:value={step}><br/>
+        <label for="interval_pie_last">Last Day</label>
+        <input type="number" id="interval_pie_last" bind:value={last}><br/>
         <label for="interval_pie_step">Steps</label>
         <input type="number" id="interval_pie_step" bind:value={steps}>
     </div>
