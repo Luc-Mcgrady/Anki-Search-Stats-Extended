0dd28ef0813a0d5a4e891d3ed07abcfb098d078a
Luc Mcgrady <lucmcgrady@gmail.com>
2023-08-07 20:05:00 +0100

    Renamed: Graphs -> Stats

--- __init__.py
+++ __init__.py
@@ -6,9 +6,9 @@ import os.path
 addon_dir = os.path.dirname(__file__)
 
 def new_refresh(self: NewDeckStats):
-    with open(f"{addon_dir}/graphs.min.js") as f: # Putting this inside the function allows you to rebuild the page with anki still open
+    with open(f"{addon_dir}/stats.min.js") as f: # Putting this inside the function allows you to rebuild the page with anki still open
         innerJs = f.read()
-    with open(f"{addon_dir}/graphs.min.css") as f: # Putting this inside the function allows you to rebuild the page with anki still open
+    with open(f"{addon_dir}/stats.min.css") as f: # Putting this inside the function allows you to rebuild the page with anki still open
         innerCss = f.read()
 
     setCss = f"const css = `{innerCss}`;"
