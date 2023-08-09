238bd1452849989241283a17b69c94c893be455c
Luc Mcgrady <lucmcgrady@gmail.com>
2023-08-09 17:52:46 +0100

    Added: Legend for bar chart

--- src/ts/Bar.svelte
+++ src/ts/Bar.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import _ from "lodash";
     import { renderBarChart, type BarChart } from "./bar";
 
     let svg: SVGElement | null = null
@@ -11,3 +12,18 @@
 </script>
 
 <svg bind:this={svg}></svg>
+
+<div class="glossary">
+    {#each _.zip(data.row_labels, data.row_colours) as [label, colour] }
+        <div>
+            <span style={`color:${colour}`}>■&nbsp;</span> {label}
+        </div>    
+    {/each}
+</div>
+
+<style>
+    div.glossary {
+        display: flex;
+        gap: 1em;
+    }
+</style>
