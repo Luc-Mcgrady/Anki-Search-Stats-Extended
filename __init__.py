5be1031c5ffb612b13bd74f4c4f78d328d1b7271
Luc Mcgrady <lucmcgrady@gmail.com>
2023-08-12 14:17:45 +0100

    Fix: Endpoint returned not json

--- __init__.py
+++ __init__.py
@@ -24,6 +24,27 @@ import json
 from aqt.mediasrv import post_handlers
 from aqt import mw
 
+def card_json(object):
+    # https://regex101.com/r/lRU1dh/1
+    return { 
+        "card_id": object.card_id,
+        "note_id": object.note_id,
+        "deck": object.deck,
+        "added": object.added,
+        "first_review": object.first_review,
+        "latest_review": object.latest_review,
+        "due_date": object.due_date,
+        "interval": object.interval,
+        "ease": object.ease,
+        "reviews": object.reviews,
+        "average_secs": object.average_secs,
+        "total_secs": object.total_secs,
+        "card_type": object.card_type,
+        "notetype": object.notetype,
+        "custom_data": object.custom_data,
+        # "revlog": Not yet implemented
+    }
+
 def card_search() -> bytes:
     search = request.data
     return Response(str(list(mw.col.find_cards(search))))
@@ -32,10 +53,9 @@ post_handlers["cardSearch"] = card_search
 
 def card_data() -> bytes:
     cards = request.data
-    print(request.data)
     cards: list[int] = json.loads(cards)
     assert isinstance(cards, list)
     assert isinstance(cards[0], int)
-    return Response(str([mw.col.card_stats_data(a) for a in cards]))
+    return Response(json.dumps([(card_json(mw.col.card_stats_data(a))) for a in cards]))
 
 post_handlers["cardData"] = card_data
