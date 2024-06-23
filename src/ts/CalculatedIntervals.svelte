25f19b10856a969994f68ae46fa208665791aa71
Luc Mcgrady <lucmcgrady@gmail.com>
2024-06-23 21:30:15 +0100

    Fix: New Pie Burdens

--- src/ts/CalculatedIntervals.svelte
+++ src/ts/CalculatedIntervals.svelte
@@ -9,22 +9,27 @@
     
     let lapses: number[] = []
     let repetitions: number[] = []
+    let lapses_burden: number[] = []
+    let repetitions_burden: number[] = []
 
     for (const card of cardData) {
         lapses[card.lapses] = (lapses[card.lapses] ?? 0) + 1
         repetitions[card.reps] = (repetitions[card.reps] ?? 0) + 1
+        if (card.ivl > 0) {
+            lapses_burden[card.lapses] = (lapses_burden[card.lapses] ?? 0) + (1 / card.ivl)
+            repetitions_burden[card.reps] = (repetitions_burden[card.reps] ?? 0) + (1 / card.ivl)
+        }
     }
-
 </script>
 
 
 <GraphContainer>
     <h1>Lapse Distribution</h1>
-    <IntervalPie spectrumFrom={"#bd3f09"} spectrumTo={"#612207"} intervals={lapses}/>
+    <IntervalPie legend_title="Lapse count: Card count" spectrumFrom={"#bd3f09"} spectrumTo={"#612207"} intervals={lapses}/>
 </GraphContainer>
 <GraphContainer>
     <h1>Lapse Load</h1>
-    <BurdenPie spectrumFrom={"#bd3f09"} spectrumTo={"#612207"} intervals={lapses}/>
+    <IntervalPie legend_title="Lapse count: Card count" spectrumFrom={"#bd3f09"} spectrumTo={"#612207"} intervals={lapses_burden}/>
 </GraphContainer>
 <GraphContainer>
     <h1>Repetition Distribution</h1>
@@ -32,5 +37,5 @@
 </GraphContainer>
 <GraphContainer>
     <h1>Repetition Load</h1>
-    <BurdenPie spectrumFrom={"#5ca7f7"} spectrumTo={"#0b4f99"} intervals={repetitions}/>
+    <IntervalPie spectrumFrom={"#5ca7f7"} spectrumTo={"#0b4f99"} intervals={repetitions_burden}/>
 </GraphContainer>
