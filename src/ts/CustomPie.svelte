c64f03f5606ca1cd2d5b583f36dcb0f132832119
Luc Mcgrady <lucmcgrady@gmail.com>
2024-06-24 20:03:51 +0100

    Added: Custom pie burden mode

--- src/ts/CustomPie.svelte
+++ src/ts/CustomPie.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     import Pie from "./Pie.svelte"
     import type { PieDatum } from "./pie"
-    import { search as doSearch } from "./search"
+    import { search as doSearch, getCardData } from "./search"
 
     export let search: string
+    export let burden_mode = false
 
     const pickable_colours = ["blue", "red", "green", "orange"]
     let pickable_colours_i = 0
@@ -14,7 +15,12 @@
 
     async function getQuery(query: string): Promise<number> {
         const result = await doSearch(`(${query})`)
-        return result.length
+        if (!burden_mode) {
+            return result.length
+        } else {
+            const cards = await getCardData(result)
+            return cards.reduce((p, n) => (p += n.ivl ? 1 / n.ivl : 0), 0)
+        }
     }
 
     async function newSearch() {
@@ -30,6 +36,16 @@
         pickable_colours_i %= pickable_colours.length
     }
 
+    async function refresh() {
+        pie_data = await Promise.all(
+            pie_data.map(async (a) => ({
+                label: a.label,
+                colour: a.colour,
+                value: await getQuery(a.label),
+            }))
+        )
+    }
+
     async function onChange(datum: PieDatum) {
         datum.value = await getQuery(datum.label)
         pie_data = [...pie_data]
@@ -39,9 +55,16 @@
         pie_data = []
         newSearch()
     }
+
+    $: legend_right = burden_mode ? "Burden" : "Cards"
 </script>
 
-<Pie data={pie_data} legend_left="Search" legend_right="Cards"></Pie>
+<label>
+    Burden:
+    <input type="checkbox" bind:checked={burden_mode} on:change={refresh} />
+</label>
+
+<Pie data={pie_data} legend_left="Search" {legend_right}></Pie>
 <div>
     <span>Search</span>
     <span>Colour</span>
