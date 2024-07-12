88b7d5748bd1f7a08bb7ee7b54be0adda8f70fee
Luc Mcgrady <lucmcgrady@gmail.com>
2024-07-12 22:54:14 +0100

    Extracted: CreateAxis

--- src/ts/bar.ts
+++ src/ts/bar.ts
@@ -13,42 +13,47 @@ export type BarChart = {
     data: BarDatum[]
 }
 
-export function renderBarChart(chart: BarChart, svg: SVGElement) {
+export function createAxis(svg: SVGElement, labels: string[], max: number, min: number) {
     const bounds = defaultGraphBounds()
     bounds.width = 500
 
     d3.select(svg).selectAll("g").remove()
 
-    const d3svg = d3
+    const axis = d3
         .select(svg)
         .attr("viewBox", `-40 -10 ${bounds.width + 50} ${bounds.height + 50}`)
         .append("g")
 
-    const x = d3
-        .scaleBand()
-        .domain(d3.map(chart.data, (d) => d.label))
-        .range([0, bounds.width])
-        .padding(0.2)
+    const x = d3.scaleBand().domain(labels).range([0, bounds.width]).padding(0.2)
 
+    const y = d3.scaleLinear().domain([max, min]).range([min, bounds.height])
+
+    axis.append("g").call(d3.axisLeft(y))
+
+    axis.append("g")
+        .attr("transform", `translate(0, ${bounds.height})`)
+        .call(d3.axisBottom(x).tickSizeOuter(0))
+
+    return { x, y, axis }
+}
+
+export function renderBarChart(chart: BarChart, svg: SVGElement) {
     const max = _.maxBy(chart.data, (d) => _.sum(Object.values(d.values)))!
     const maxValue = _.sum(Object.values(max.values))
 
-    const y = d3.scaleLinear().domain([maxValue, 0]).range([0, bounds.height])
-
-    d3svg.append("g").call(d3.axisLeft(y))
-
-    d3svg
-        .append("g")
-        .attr("transform", `translate(0, ${bounds.height})`)
-        .call(d3.axisBottom(x).tickSizeOuter(0))
+    const { axis, x, y } = createAxis(
+        svg,
+        chart.data.map((datum) => datum.label),
+        maxValue,
+        0
+    )
 
     const stack = d3
         .stack<BarDatum, number>()
         .keys(_.range(0, chart.row_labels.length))
         .value((obj, key) => obj.values[key])(chart.data)
 
-    d3svg
-        .append("g")
+    axis.append("g")
         .selectAll("g")
         .data(stack)
         .join("g")
@@ -61,7 +66,7 @@ export function renderBarChart(chart: BarChart, svg: SVGElement) {
         .attr("height", (d) => y(d[0]) - y(d[1]))
         .attr("width", x.bandwidth())
 
-    return { x, y, svg: d3svg, maxValue }
+    return { x, y, svg: axis, maxValue }
 }
 
 export type ExtraRenderInput = ReturnType<typeof renderBarChart>
