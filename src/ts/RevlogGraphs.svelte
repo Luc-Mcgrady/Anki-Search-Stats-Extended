c7337d0160d26a266eb9401ea28169c55b866d8d
Luc Mcgrady <lucmcgrady@gmail.com>
2024-07-12 17:21:23 +0100

    Fix: Graph doesn't update on search

--- src/ts/RevlogGraphs.svelte
+++ src/ts/RevlogGraphs.svelte
@@ -4,21 +4,24 @@
     import type { Revlog } from "./search"
 
     export let revlog_data: Revlog[]
-    let card_times: Record<number, number> = {}
-    let card_counts: Record<number, number> = {}
-    let revlog_times: number[] = []
+    let card_times: Record<number, number>
+    //let card_counts: Record<number, number> = {}
+    let revlog_times: number[]
 
-    for (const revlog of revlog_data) {
-        card_times[revlog.cid] = (card_times[revlog.cid] ?? 0) + revlog.time
-        //card_counts[revlog.cid] = (card_times[revlog.cid] ?? 0) + 1
+    $: {
+        card_times = {}
+        revlog_times = []
+
+        for (const revlog of revlog_data) {
+            card_times[revlog.cid] = (card_times[revlog.cid] ?? 0) + revlog.time
+            //card_counts[revlog.cid] = (card_times[revlog.cid] ?? 0) + 1
+        }
+
+        for (const card_time of Object.values(card_times)) {
+            const key = Math.floor(card_time / 1000)
+            revlog_times[key] = (revlog_times[key] ?? 0) + 1
+        }
     }
-
-    for (const card_time of Object.values(card_times)) {
-        const key = Math.floor(card_time / 1000)
-        revlog_times[key] = (revlog_times[key] ?? 0) + 1
-    }
-
-    console.log({ revlog_data, card_times, revlog_times })
 </script>
 
 <GraphContainer>
@@ -27,7 +30,6 @@
         intervals={revlog_times}
         spectrumFrom={"#fcba03"}
         spectrumTo={"#543e00"}
-        totalDescriptor={""}
         last={59}
         steps={10}
         fillerColour={"blue"}
