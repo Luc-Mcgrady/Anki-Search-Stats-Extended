772de5ad7f46ce6db952a9bf838a0fb98f742462
Luc Mcgrady <lucmcgrady@gmail.com>
2024-07-14 12:14:54 +0100

    Neatened svelte store types

--- src/ts/stores.ts
+++ src/ts/stores.ts
@@ -1,23 +1,23 @@
-import { writable, type Writable } from "svelte/store"
+import { writable } from "svelte/store"
 import type { GraphsResponse } from "./proto/anki/stats_pb"
 import type { CardData, Revlog } from "./search"
 import type { Tooltip } from "./tooltip"
 
-export let data: Writable<null | GraphsResponse> = writable(null)
-export let not_suspended_data: Writable<null | GraphsResponse> = writable(null)
-export let mature_data: Writable<null | GraphsResponse> = writable(null)
-export let learn_data: Writable<null | GraphsResponse> = writable(null)
-export let relearn_data: Writable<null | GraphsResponse> = writable(null)
+export let data = writable<null | GraphsResponse>(null)
+export let not_suspended_data = writable<null | GraphsResponse>(null)
+export let mature_data = writable<null | GraphsResponse>(null)
+export let learn_data = writable<null | GraphsResponse>(null)
+export let relearn_data = writable<null | GraphsResponse>(null)
 
-export let searchString: Writable<null | string> = writable(null)
-export let card_data: Writable<null | CardData[]> = writable(null)
-export let revlogs: Writable<null | Revlog[]> = writable(null)
+export let searchString = writable<null | string>(null)
+export let card_data = writable<null | CardData[]>(null)
+export let revlogs = writable<null | Revlog[]>(null)
 
 export let include_suspended = writable(false)
 export let zero_inclusive = writable(false)
 
 export let burdenOrLoad = writable("Load")
-export let tooltip: Writable<Tooltip> = writable({ shown: false })
+export let tooltip = writable<Tooltip>({ shown: false })
 
 export let config = writable<SSEconfig>()
 export let other = writable<SSEother>()
