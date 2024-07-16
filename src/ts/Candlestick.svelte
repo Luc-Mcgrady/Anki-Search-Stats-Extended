587f756659568733d95794c24db50b50d86b39bb
Luc Mcgrady <lucmcgrady@gmail.com>
2024-07-16 14:34:11 +0100

    Fix: Candlestick style inconsistent

--- src/ts/Candlestick.svelte
+++ src/ts/Candlestick.svelte
@@ -49,3 +49,19 @@
 </div>
 
 <svg bind:this={svg}></svg>
+
+<style>
+    div.options {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 0.5em 1em;
+        align-items: baseline;
+    }
+
+    label {
+        display: grid;
+        align-items: baseline;
+        white-space: nowrap;
+        gap: 0.5em;
+    }
+</style>
