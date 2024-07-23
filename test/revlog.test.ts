1edffd49b45d639105f44a82d956e542a4ea9fd8
Luc Mcgrady <lucmcgrady@gmail.com>
2024-07-23 13:30:49 +0100

    Test: second revlog card

--- test/revlog.test.ts
+++ test/revlog.test.ts
@@ -50,28 +50,32 @@ class RevlogBuilder {
     }
 }
 
-const burden_revlog_builder = new RevlogBuilder()
+const burden_revlog_builder1 = new RevlogBuilder()
+const burden_revlog_builder2 = new RevlogBuilder()
 const burden_revlogs : Revlog[] = [
-    burden_revlog_builder.review(-5000),
-    burden_revlog_builder.review(-6000),
-    burden_revlog_builder.review(1),
-    burden_revlog_builder.review(2),
-    burden_revlog_builder.review(0),
-    burden_revlog_builder.wait(2 * day_ms) as Revlog,
-    burden_revlog_builder.review(-5000),
-    burden_revlog_builder.review(-6000),
-    burden_revlog_builder.review(1),
-    burden_revlog_builder.review(4)
+    burden_revlog_builder1.review(-5000),
+    burden_revlog_builder1.review(-6000),
+    burden_revlog_builder1.review(1),
+    burden_revlog_builder1.review(2),
+    burden_revlog_builder1.review(0),
+    burden_revlog_builder1.wait(2 * day_ms) as Revlog,
+    burden_revlog_builder1.review(-5000),
+    burden_revlog_builder1.review(-6000),
+    burden_revlog_builder1.review(1),
+    burden_revlog_builder1.review(4),
+
+    burden_revlog_builder2.wait(7*day_ms) as Revlog,
+    burden_revlog_builder2.review(1) as Revlog,
 ]
 
 // console.log(burden_revlogs.map(revlog=>({id: revlog.id / day_ms, ...revlog})))
 
-const {burden_change, burden} = calculateRevlogStats(burden_revlogs, [burden_revlog_builder.card()] as any, 8)
+const {burden_change, burden} = calculateRevlogStats(burden_revlogs, [burden_revlog_builder1.card(), burden_revlog_builder2.card()] as any, 8)
 
 test("Burden", () =>{
-    expect(burden).toMatchObject([1, 0.5, 0.5, 0, 0, 1, 0.25, 0.25, 0.25])
+    expect(burden).toMatchObject([1, 0.5, 0.5, 0, 0, 1, 1.25, 1.25, 1.25])
 })
 
 test("Burden delta", () =>{
-    expect(burden_change).toMatchObject([1, -0.5, 0, -0.5, 0 , 1, -0.75, 0, 0])
+    expect(burden_change).toMatchObject([1, -0.5, 0, -0.5, 0 , 1, 0.25, 0, 0])
 })
