87884b5b840f3f084a49b209eeca94622316b5fa
Luc Mcgrady <lucmcgrady@gmail.com>
2024-07-23 12:05:55 +0100

    Test: Intra-day revlogs

--- test/revlog.test.ts
+++ test/revlog.test.ts
@@ -18,8 +18,9 @@ class RevlogBuilder {
     }
 
     build(ivl: number, time_spent: number = 0, ease: 0 | 1 | 2 | 3 | 4 = 0, relearn: boolean = false) {
-        const out: Revlog = {
-            id: this.state.id + ivl * day_ms,
+
+        this.state = {
+            id: this.state.id + (ivl > 0 ? ivl * day_ms : -ivl),
             cid: this.state.cid,
             ivl,
             ease,
@@ -30,9 +31,15 @@ class RevlogBuilder {
             type: ivl == 0 ? 4 : (relearn ? 2 : 1)
         }
 
-        this.state = out
+        return this.state as Revlog
+    }
 
-        return out
+    wait(ms: number) {
+        this.state = {
+            id: this.state.id + ms,
+            ...this.state
+        }
+        return this.state
     }
 
     card() {
@@ -43,14 +50,27 @@ class RevlogBuilder {
     }
 }
 
-const burden_test_builder = new RevlogBuilder()
-const burden_test = [burden_test_builder.build(1), burden_test_builder.build(2)]
+const burden_revlog_builder = new RevlogBuilder()
+const burden_revlogs : Revlog[] = [
+    burden_revlog_builder.build(-5000),
+    burden_revlog_builder.build(-6000),
+    burden_revlog_builder.build(1),
+    burden_revlog_builder.build(2),
+    burden_revlog_builder.build(0),
+    burden_revlog_builder.wait(2 * day_ms) as Revlog,
+    burden_revlog_builder.build(-5000),
+    burden_revlog_builder.build(1),
+    burden_revlog_builder.build(2)
+]
 
-console.log(burden_test)
+// console.log(burden_revlogs)
 
-const {burden_change, burden} = calculateRevlogStats(burden_test, [burden_test_builder.card()] as any, 2)
+const {burden_change, burden} = calculateRevlogStats(burden_revlogs, [burden_revlog_builder.card()] as any, 8)
 
-test("Burden data", () =>{
+test("Burden", () =>{
     expect(burden).toMatchObject([1, 1, 0.5])
+})
+
+test("Burden delta", () =>{
     expect(burden_change).toMatchObject([1, 0, -0.5])
 })
