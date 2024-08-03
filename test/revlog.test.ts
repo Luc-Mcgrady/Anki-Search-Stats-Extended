ef231ef4d2a9f9ab6e54a6a6ecb9051f9ac2387b
Luc Mcgrady <lucmcgrady@gmail.com>
2024-08-03 22:06:09 +0100

    Test: Learning cards

--- test/revlog.test.ts
+++ test/revlog.test.ts
@@ -58,6 +58,8 @@ const burden_revlogs : Revlog[] = [
 
     burden_revlog_builder2.wait(7*day_ms) as Revlog,
     burden_revlog_builder2.review(1) as Revlog,
+    burden_revlog_builder2.wait(2*day_ms) as Revlog,
+    burden_revlog_builder2.review(-5000) as Revlog,
 ]
 
 // console.log(burden_revlogs.map(revlog=>({id: revlog.id / day_ms, ...revlog})))
