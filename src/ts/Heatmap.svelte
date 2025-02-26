9a5262a47448d3943ba9582e1eb94a1a949027a6
Ross Brown <rbrownwsws@googlemail.com>
2025-02-26 15:17:13 +0000

    Twiddle with tooltip

--- src/ts/Heatmap.svelte
+++ src/ts/Heatmap.svelte
@@ -146,8 +146,9 @@
                     x: window.scrollX + target_rect.x,
                     y: window.scrollY + target_rect.y + target_rect.height + 16,
                     text: [
-                        `${yTooltipLabel}: ${yTooltipFormat.format(y_from)}-${yTooltipFormat.format(y_to)}`,
-                        `${xTooltipLabel}: ${xTooltipFormat.format(x_from)}-${xTooltipFormat.format(x_to)}`,
+                        // N.B. en dashes not hyphens
+                        `${yTooltipLabel}: ${yTooltipFormat.format(y_from)} – ${yTooltipFormat.format(y_to)}`,
+                        `${xTooltipLabel}: ${xTooltipFormat.format(x_from)} – ${xTooltipFormat.format(x_to)}`,
                         `${valueTooltipLabel}: ${valueTooltipFormat.format(value)}`,
                     ],
                 }
