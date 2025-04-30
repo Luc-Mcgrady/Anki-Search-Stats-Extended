570e05d509b21c1a15a984f6b5c7c441feb596ac
Luc Mcgrady <lucmcgrady@gmail.com>
2025-04-30 11:45:40 +0100

    Fix: Weights not passed to tests

--- test/memorised.test.ts
+++ test/memorised.test.ts
@@ -6,11 +6,11 @@ import {fsrs} from "ts-fsrs"
 
 const weights = [
     0.40255, 1.18385, 3.173, 15.69105, 7.1949, 0.5345, 1.4604, 0.0046, 1.54575, 0.1192, 1.01925,
-    1.9395, 0.11, 0.29605, 2.2698, 0.2315, 2.9898, 0.51655, 0.6621, 0, 0.2, // Defaults
+    1.9395, 0.11, 0.29605, 2.2698, 0.2315, 2.9898, 0.51655, 0.6621, // Defaults
 ]
 const weights2 = [
     1, 100, 100, 100, 7.1949, 0.5345, 1.4604, 0.0046, 1.54575, 0.1192, 1.01925,
-    1.9395, 0.11, 0.29605, 2.2698, 0.2315, 2.9898, 0, 0, 0, 0.2 // on initial good: 100, no intra-day
+    1.9395, 0.11, 0.29605, 2.2698, 0.2315, 2.9898 // on initial good: 100, no intra-day
 ]
 
 const mappings = {1: 1, 2: 2}
@@ -49,7 +49,7 @@ test("Day Timings", ()=>{
 // https://github.com/open-spaced-repetition/fsrs-rs/blob/a7aaa40498bae992e0be0a1e9a1380e4992aee60/src/inference.rs#L433-L465
 test("Stability", ()=>{
     const card = new RevlogBuilder()
-    const FSRS = fsrs()
+    const FSRS = fsrs({w: weights})
 
     const revlogs = [
         card.review(1, 1),
@@ -73,7 +73,7 @@ test("Stability", ()=>{
 
 test("Stability On Forget", ()=>{
     const card = new RevlogBuilder()
-    const FSRS = fsrs()
+    const FSRS = fsrs({w: weights})
 
     const revlogs = [
         card.review(1, 1),
