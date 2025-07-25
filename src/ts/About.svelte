05152d66dc4c98015ffe6fea66fbad2e36c9ce77
Luc Mcgrady <lucmcgrady@gmail.com>
2025-07-25 18:26:43 +0100

    Translation section

--- src/ts/About.svelte
+++ src/ts/About.svelte
@@ -24,13 +24,23 @@
         <h1>{i18n("translate")}</h1>
         Search Stats Extended is currently has localisations for
         <code>{currently_translated}</code>
+        If you want to translate this addon
         <br />
-        Make a file for your language and create a pull request on github!
-        <code>"View Files/locale/{SSEother.lang}.ftl"</code>
+        Make a file for your language ({SSEother.lang}) in
+        <code>
+            {`(Addon menu -> Search Stats Extended -> View Files)/locale/${SSEother.lang}.ftl`}
+        </code>
+        Copy in the original translations from
+        <code>en_GB.ftl</code>
+        translate it and create a pull request on github!
+        <br />
         <a
             href={`https://github.com/Luc-Mcgrady/Anki-Search-Stats-Extended/new/main/locale?filename=${SSEother.lang}.ftl`}
         >
             <button class="btn">Submit to github</button>
+        </a>
+        <a href="https://github.com/Luc-Mcgrady/Anki-Search-Stats-Extended/tree/main/locale">
+            <button class="btn">Edit existing translations</button>
         </a>
     </GraphContainer>
     <GraphContainer>
@@ -44,4 +54,12 @@
 </GraphCategory>
 
 <style lang="scss">
+    .btn {
+        margin: 0.25em;
+        background-color: #0d6efd;
+        &:hover {
+            background-color: #0b5ed7;
+            color: var(--bs-button-color);
+        }
+    }
 </style>
