fd22843ccf55947e38481c6d30ccaf43ffa56e7f
Luc Mcgrady <lucmcgrady@gmail.com>
2025-07-25 18:33:15 +0100

    Extract LinkButton

--- src/ts/About.svelte
+++ src/ts/About.svelte
@@ -2,6 +2,7 @@
     import GraphCategory from "./GraphCategory.svelte"
     import GraphContainer from "./GraphContainer.svelte"
     import { i18n } from "./i18n"
+    import LinkButton from "./LinkButton.svelte"
 
     let currently_translated = ["en-GB", "zh_CN"]
 </script>
@@ -34,32 +35,23 @@
         <code>en_GB.ftl</code>
         translate it and create a pull request on github!
         <br />
-        <a
+        <LinkButton
             href={`https://github.com/Luc-Mcgrady/Anki-Search-Stats-Extended/new/main/locale?filename=${SSEother.lang}.ftl`}
         >
-            <button class="btn">Submit to github</button>
-        </a>
-        <a href="https://github.com/Luc-Mcgrady/Anki-Search-Stats-Extended/tree/main/locale">
-            <button class="btn">Edit existing translations</button>
-        </a>
+            Submit to github
+        </LinkButton>
+        <LinkButton
+            href={`https://github.com/Luc-Mcgrady/Anki-Search-Stats-Extended/tree/main/locale`}
+        >
+            Edit existing translations
+        </LinkButton>
     </GraphContainer>
     <GraphContainer>
         <h1>{i18n("support")}</h1>
-        Be sure to leave a thumbs up on AnkiWeb and to star on Github
+        <a href="https://ankiweb.net/shared/info/1613056169">Like on ankiweb</a>
     </GraphContainer>
     <GraphContainer>
         <h1>{i18n("config")}</h1>
         You can find more options to customise your experience in the config Try
     </GraphContainer>
 </GraphCategory>
-
-<style lang="scss">
-    .btn {
-        margin: 0.25em;
-        background-color: #0d6efd;
-        &:hover {
-            background-color: #0b5ed7;
-            color: var(--bs-button-color);
-        }
-    }
-</style>
---
+++ src/ts/LinkButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let href: string
+</script>
+
+<a {href}><button class="btn"><slot /></button></a>
+
+<style lang="scss">
+    .btn {
+        margin: 0.25em;
+        background-color: #0d6efd;
+        &:hover {
+            background-color: #0b5ed7;
+            color: var(--bs-button-color);
+        }
+    }
+</style>
