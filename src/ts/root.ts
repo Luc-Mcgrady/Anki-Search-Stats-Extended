bc6894ddbebae0e6f65c65bb4b2b17abd972ed31
user1823 <92206575+user1823@users.noreply.github.com>
2026-03-16 05:58:32 +0530

    Fix shared mutable state race condition in fetchSwappedSearch (#101)
    * Fix shared mutable state race condition in fetchSwappedSearch
    
    * no need to reset headers
    
    ---------
    
    Co-authored-by: Luc Mcgrady <lucmcgrady@gmail.com>

--- src/ts/root.ts
+++ src/ts/root.ts
@@ -52,8 +52,8 @@ let origReq: string = ""
 let origHeaders: any = { body: undefined }
 
 export function fetchSwappedSearch(criteria: string, limit?: number) {
-    origHeaders.body = bodySwap(origBody, criteria, limit)
-    return fetchAndDecode(realFetch(origReq, origHeaders))
+    const headers = { ...origHeaders, body: bodySwap(origBody, criteria, limit) }
+    return fetchAndDecode(realFetch(origReq, headers))
 }
 
 export function patchFetch() {
@@ -76,8 +76,6 @@ export function patchFetch() {
             const limit = get(alwaysAllTime) ? 0 : undefined
             fetchSwappedSearch("", limit).then(data.set)
             fetchSwappedSearch("-is:suspended", limit).then(not_suspended_data.set)
-
-            headers.body = origBody
         }
         return realFetch(req, headers)
     }
